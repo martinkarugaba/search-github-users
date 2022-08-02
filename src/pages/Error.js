dc303def85e84754064b309afc6a3ec273d1cbe9
Martin Karugaba <martinkarugaba21@gmail.com>
2022-08-02 16:59:02 +0300

    set up github context

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,9 +1,21 @@
-import React from 'react';
-import styled from 'styled-components';
-import { Link } from 'react-router-dom';
+import React from "react";
+import styled from "styled-components";
+import {Link} from "react-router-dom";
+
 const Error = () => {
-  return <h2>error page</h2>;
+  return (
+    <Wrapper>
+      <div>
+        <h1>404</h1>
+        <h4>sorry the page you tried cannot be found</h4>
+        <Link to='/' className='btn'>
+          back home
+        </Link>
+      </div>
+    </Wrapper>
+  );
 };
+
 const Wrapper = styled.section`
   min-height: 100vh;
   display: grid;
