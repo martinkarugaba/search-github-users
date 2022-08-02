83f3b6c898dddd6b20fb488e4b7ecb5d58235574
Martin Karugaba <martinkarugaba21@gmail.com>
2022-08-02 17:24:15 +0300

    set up useReducer hook

--- src/context/context.js
+++ src/context/context.js
@@ -1,19 +1,29 @@
-import React, { useState, useEffect } from 'react';
-import mockUser from './mockData.js/mockUser';
-import mockRepos from './mockData.js/mockRepos';
-import mockFollowers from './mockData.js/mockFollowers';
-import axios from 'axios';
+import React, {useState, useEffect, useReducer} from "react";
+import mockUser from "./mockData.js/mockUser";
+import mockRepos from "./mockData.js/mockRepos";
+import mockFollowers from "./mockData.js/mockFollowers";
+import axios from "axios";
+import reducer from './reducer'
 
-const rootUrl = 'https://api.github.com';
-
-
-
+const rootUrl = "https://api.github.com";
 const GithubContext = React.createContext();
+
+const initialState = {
+  gitHubUser: mockUser,
+  repos: mockRepos,
+  followers: mockFollowers,
+};
 
 // Provider, Consumer = GithubContext.Provider
 
 const GithubProvider = ({children}) => {
-  return <GithubContext.Provider value={'hello'}>{children}</GithubContext.Provider>
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  return (
+    <GithubContext.Provider value={{...state}}>
+      {children}
+    </GithubContext.Provider>
+  );
 };
 
 export {GithubProvider, GithubContext};
