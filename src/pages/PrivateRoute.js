365449fc259274ec6faba0e345f644c6f7ddf35b
martin karugaba <martinkarugaba21@gmail.com>
2022-08-23 02:51:25 +0300

    fixed login page css

--- src/pages/PrivateRoute.js
+++ src/pages/PrivateRoute.js
@@ -2,16 +2,11 @@ import React from 'react';
 import { Navigate } from 'react-router-dom';
 import { useAuth0 } from '@auth0/auth0-react';
 
-const PrivateRoute = ({children}) => {
+const PrivateRoute = ({ children }) => {
   const { isAuthenticated, user } = useAuth0();
   const isUser = isAuthenticated && user;
 
-  //if (!isUser) {
-  //  <Navigate to="/login" />;
-  //}
-  //return children;
-
-  return isUser ? children : <Navigate to="/login" />
+  return isUser ? children : <Navigate to="/login" />;
 };
 
 export default PrivateRoute;
